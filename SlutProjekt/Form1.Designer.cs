5f80ec7e6c2acb12a3d343e3cc1bb28d40aca393
Eddie Gbalay <0226gbdd@jonkoping.se>
2021-04-12 09:15:35 +0200

    La till all tabs som jag ska använda lekte runt med lite olika designer i början. Visste inte om jag skulle använda mig av en split container men valde att köra på tabs till slut. Tog lite mer tid än vad jag trodde. Ska sitta och jobba med gränsnitten på dem olika sidorna nu framöver.

--- SlutProjekt/Form1.Designer.cs
+++ SlutProjekt/Form1.Designer.cs
@@ -29,13 +29,87 @@ namespace SlutProjekt
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabHome = new System.Windows.Forms.TabPage();
+            this.tabTickets = new System.Windows.Forms.TabPage();
+            this.tabCustomers = new System.Windows.Forms.TabPage();
+            this.tabFilms = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabHome);
+            this.tabControl1.Controls.Add(this.tabTickets);
+            this.tabControl1.Controls.Add(this.tabFilms);
+            this.tabControl1.Controls.Add(this.tabCustomers);
+            this.tabControl1.Enabled = false;
+            this.tabControl1.Location = new System.Drawing.Point(12, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 435);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabHome
+            // 
+            this.tabHome.Location = new System.Drawing.Point(4, 22);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHome.Size = new System.Drawing.Size(768, 409);
+            this.tabHome.TabIndex = 0;
+            this.tabHome.Text = "Hem";
+            this.tabHome.UseVisualStyleBackColor = true;
+            // 
+            // tabTickets
+            // 
+            this.tabTickets.Location = new System.Drawing.Point(4, 22);
+            this.tabTickets.Name = "tabTickets";
+            this.tabTickets.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTickets.Size = new System.Drawing.Size(768, 409);
+            this.tabTickets.TabIndex = 1;
+            this.tabTickets.Text = "Biljetter";
+            this.tabTickets.UseVisualStyleBackColor = true;
+            // 
+            // tabCustomers
+            // 
+            this.tabCustomers.Location = new System.Drawing.Point(4, 22);
+            this.tabCustomers.Name = "tabCustomers";
+            this.tabCustomers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCustomers.Size = new System.Drawing.Size(768, 409);
+            this.tabCustomers.TabIndex = 2;
+            this.tabCustomers.Text = "Medlem";
+            this.tabCustomers.UseVisualStyleBackColor = true;
+            // 
+            // tabFilms
+            // 
+            this.tabFilms.Location = new System.Drawing.Point(4, 22);
+            this.tabFilms.Name = "tabFilms";
+            this.tabFilms.Padding = new System.Windows.Forms.Padding(3);
+            this.tabFilms.Size = new System.Drawing.Size(768, 409);
+            this.tabFilms.TabIndex = 3;
+            this.tabFilms.Text = "Filmer";
+            this.tabFilms.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabHome;
+        private System.Windows.Forms.TabPage tabTickets;
+        private System.Windows.Forms.TabPage tabFilms;
+        private System.Windows.Forms.TabPage tabCustomers;
     }
 }
 
